86f7ce8d9e0a13d0552b2ab3a87948b11e1a6098
inakarune <inakarune@naver.com>
2016-10-30 23:27:00 +0900

    Create codewars_Ookkk, Ok, O? Ook, Ok, Ooo!.js

---
+++ codewars_Ookkk, Ok, O? Ook, Ok, Ooo!.js
@@ -0,0 +1,27 @@
+function okkOokOo(s){
+  let str = s.replace('!', '').split('? ');
+  let txt = '';
+  let l = str.length;
+  
+  for(let i = 0; i < l; ++i){
+    txt += convert(str[i]);
+  }
+  console.log(str)
+  
+  
+  
+  
+  return txt;
+  function convert(str){
+    
+    str = str.replace(/, /g, '').toLowerCase().replace(/[ok]/g, function(v){
+    if(v == 'o'){
+      return '0';
+    } else if(v == 'k'){
+      return '1';
+    }
+  })
+  let num = parseInt(str, 2);
+  return String.fromCharCode(num);
+  }
+  
