c2d328d81788949ce101acd2c36603e43a23823f
inakarune <inakarune@naver.com>
2016-11-20 18:13:10 -0600

    Update codewars_Iq_test.js

--- codewars_Iq_test.js
+++ codewars_Iq_test.js
@@ -1,9 +1,9 @@
 function iqTest(numbers){
-  var arr = numbers.split(' ');
+  let arr = numbers.split(' ');
       arr.unshift(' ');
       arr.join('')
-  var even = numbers.split(' ').filter((v) => Number(v) % 2 === 0);
-  var odd = numbers.split(' ').filter((v) => Number(v) % 2 !== 0);
+  let even = numbers.split(' ').filter((v) => Number(v) % 2 === 0);
+  let odd = numbers.split(' ').filter((v) => Number(v) % 2 !== 0);
   
   return even.length > odd.length ? arr.indexOf(odd[0]) : arr.indexOf(even[0]);
 }
