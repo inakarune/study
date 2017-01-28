aa8b1b2534c4448f3c0d29086401b90957cc6e78
inakarune <inakarune@naver.com>
2017-01-28 12:49:57 +0900

    Update codewars_Even_Odd_pattern#1.js

--- codewars_Even_Odd_pattern#1.js
+++ codewars_Even_Odd_pattern#1.js
@@ -2,7 +2,7 @@ function EvenOdd(arr){
 let sum = arr[0];
   for(let i = 1; i < arr.length; i++){
     if(i % 2 == 1){
-    sum *= arr[i] 
+    sum *= arr[i]; 
     } else {
     sum += arr[i];
     }
