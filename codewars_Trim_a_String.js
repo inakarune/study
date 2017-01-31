d217d9cdd922c39085f2fe069ca1832122bbb267
inakarune <inakarune@naver.com>
2017-01-30 16:39:08 -0800

    Update codewars_Trim_a_String.js

--- codewars_Trim_a_String.js
+++ codewars_Trim_a_String.js
@@ -1,16 +1,16 @@
 String.prototype.trim = function() {
 var str = this;
-str = str.split('')
-console.log(str)
+str = str.split('');
+
 while(true){
-if (str[0] == ' ' || str[0] == '\n' || str[0] == '\t' || str[0] == '\r'){
-  str.shift();
-} else if (str[str.length - 1] == ' ' || str[str.length - 1] == '\n' || str[str.length - 1] == '\t' || str[str.length - 1] == '\r'){
-  str.pop();
-}
-if (str[0] !== ' ' && str[0] !== '\n' && str[0] !== '\t' && str[0] !== '\r' && str[str.length - 1] !== ' ' && str[str.length - 1] !== '\n' && str[str.length - 1] !== '\t' && str[str.length - 1] !== '\r'){
-  break;
-}
+  if (str[0] == ' ' || str[0] == '\n' || str[0] == '\t' || str[0] == '\r'){
+    str.shift();
+  } else if (str[str.length - 1] == ' ' || str[str.length - 1] == '\n' || str[str.length - 1] == '\t' || str[str.length - 1] == '\r'){
+    str.pop();
+  }
+  if (str[0] !== ' ' && str[0] !== '\n' && str[0] !== '\t' && str[0] !== '\r' && str[str.length - 1] !== ' ' && str[str.length - 1] !== '\n' && str[str.length - 1] !== '\t' && str[str.length - 1] !== '\r'){
+    break;
+  }
 }
   return str.join('');
 };
